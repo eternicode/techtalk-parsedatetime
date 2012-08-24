85f0a0ed8eddfd1a3959d45f582874696fcb8fff
Andrew Rowls <andrew@eternicode.com>
2012-08-23 22:26:17 -0400

    Project setup

---
+++ pdt/settings.py
@@ -0,0 +1,97 @@
+# Django settings for pdt project.
+
+from os.path import join, abspath, dirname
+
+ROOT_DIR = dirname(abspath(__file__))
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = ()
+MANAGERS = ADMINS
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': join(ROOT_DIR, 'sqlite3.db'),
+    }
+}
+
+TIME_ZONE = 'America/Chicago'
+LANGUAGE_CODE = 'en-us'
+
+SITE_ID = 1
+
+USE_I18N = False
+USE_L10N = False
+USE_TZ = False
+
+MEDIA_ROOT = join(ROOT_DIR, 'media')
+MEDIA_URL = '/media/'
+
+STATIC_ROOT = join(ROOT_DIR, 'static')
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    join(ROOT_DIR, 'static_source'),
+)
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+
+SECRET_KEY = '^wzdc6zwn-qr%x=une&amp;!kd_jg6l19_u+)tb9eh)w=_lo+wx^5$'
+
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+)
+
+ROOT_URLCONF = 'pdt.urls'
+WSGI_APPLICATION = 'pdt.wsgi.application'
+
+TEMPLATE_DIRS = ()
+
+INSTALLED_APPS = (
+    #'django.contrib.auth',
+    #'django.contrib.contenttypes',
+    #'django.contrib.sessions',
+    #'django.contrib.sites',
+    #'django.contrib.messages',
+    'django.contrib.staticfiles',
+    # Uncomment the next line to enable the admin:
+    # 'django.contrib.admin',
+)
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
